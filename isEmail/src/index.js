507f6a4a2d32ebe5d6d86ca661943fa2f1031442
vorillaz <theodorevorillas@gmail.com>
2020-10-30 20:40:50 +0200

    feat(isEmail): init

---
+++ isEmail/src/index.js
@@ -0,0 +1,15 @@
+const reg = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+
+export const isEmail = email => {
+  if (!email) {
+    return false;
+  }
+  // Block regex bombing
+  if (email.length > 254) {
+    return false;
+  }
+  if (email.match(reg) === null) {
+    return false;
+  }
+  return true;
+};
