d1ec34ccce37020f64ed77d3dc7bc4826c6c743e
vorillaz <theodorevorillas@gmail.com>
2020-12-22 19:28:38 +0200

    fix: dependencies & downloads with redirects

--- utils.js
+++ utils.js
@@ -5,30 +5,36 @@ const path = require('path');
 const download = (url, pkg) => {
   const {name, ext} = path.parse(url);
   return new Promise((resolve, reject) => {
-    const file = fs.createWriteStream(
-      path.resolve(__dirname, pkg, 'tmp', `${name}.txt`),
-      {flags: 'w'},
-    );
     let responseSent = false; // flag to make sure that response is sent only once.
     http
       .get(url, response => {
-        console.log(`> Validated download ${url}`);
-        response.pipe(file);
-        file.on('finish', () => {
-          console.log(
-            `> Download file done ${path.resolve(
-              __dirname,
-              pkg,
-              'tmp',
-              `${name}.txt`,
-            )}`,
+        if (response.statusCode >= 200 && response.statusCode < 300) {
+          const file = fs.createWriteStream(
+            path.resolve(__dirname, pkg, 'tmp', `${name}.txt`),
+            {flags: 'w'},
           );
-          file.close(() => {
-            if (responseSent) return;
-            responseSent = true;
-            resolve();
+          console.log(`> Validated download ${url}`);
+          response.pipe(file);
+          file.on('finish', () => {
+            console.log(
+              `> Download file done ${path.resolve(
+                __dirname,
+                pkg,
+                'tmp',
+                `${name}.txt`,
+              )}`,
+            );
+            file.close(() => {
+              if (responseSent) return;
+              responseSent = true;
+              resolve();
+            });
           });
-        });
+        } else if (response.headers.location) {
+          resolve(download(response.headers.location, pkg));
+        } else {
+          reject(new Error(response.statusCode + ' ' + response.statusMessage));
+        }
       })
       .on('error', err => {
         if (responseSent) return;
