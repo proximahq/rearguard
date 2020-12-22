82a3e58590152ef7ac2d8b2dba05bd63b7d5b309
vorillaz <theodorevorillas@gmail.com>
2020-12-22 22:06:38 +0200

    chore: enhance specs

--- isEmail/tests/index.js
+++ isEmail/tests/index.js
@@ -8,10 +8,13 @@ test('isEmail', t => {
   t.falsy(isEmail(`${longemail}@gmail.com`));
   t.falsy(isEmail(`@gmail.com`));
   t.truthy(isEmail('hello@gmail.com'));
+  t.truthy(isEmail('hello@gmail.co.uk'));
   t.truthy(isEmail('hello@sienna12bourne.ga'));
   t.truthy(isEmail('bar@mail2000.ml'));
   t.truthy(isEmail('a@b.com'));
   t.truthy(isEmail('valid@about.museum'));
+  t.truthy(isEmail('test123@hello.museum'));
+  t.truthy(isEmail('test-----123@hello.museum'));
   t.truthy(isEmail('~@iana.org'));
   t.truthy(isEmail('theoooodore+123@hello.org'));
   t.truthy(isEmail('first.last@123.iana.org	'));
