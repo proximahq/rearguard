e2cc4de43b046f6534115331b587e84ff77c80a2
Theodore <vorillas@Theodores-MacBook-Air.local>
2021-10-25 19:49:18 +0300

    fix: add typings for add in batch

--- batch/index.d.ts
+++ batch/index.d.ts
@@ -1,10 +1,29 @@
+/**
+ * From https://github.com/sindresorhus/type-fest/
+ * Matches any valid JSON value.
+ */
+type value = string | number | boolean | JsonObject | JsonArray | null;
+
+/**
+ * From https://github.com/sindresorhus/type-fest/
+ * Matches a JSON object.
+ * This type can be useful to enforce some input to be JSON-compatible or as a super-type to be extended from.
+ */
+type JsonObject = {[Key in string]?: value};
+
+/**
+ * From https://github.com/sindresorhus/type-fest/
+ * Matches a JSON array.
+ */
+interface JsonArray extends Array<value> {}
+
 declare function batchLru(
   maxItems: number,
   ttl: number,
-  callback: (any) => any,
+  callback: (items: JsonArray) => void,
   timeout?: number,
-): boolean;
+): {add: (item: value) => void};
 
 // EXPORTS //
 
-export = batchLru;
+export = {batchLru};
